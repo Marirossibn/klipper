Marirossibn/klipper

commit d98b1a2021d6c5039b798e305d59c5453630cb9b

gcode: Use regular str() types for g-code commands on Python2
Avoid using unicode() types on Python2 as it can lead to subtle
errors.

Also, accept utf8 on gcode input (instead of just ascii).

Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

klippy/gcode.py
@@ -381,13 +381,13 @@ class GCodeIO:
     def _process_data(self, eventtime):
         # Read input, separate by newline, and add to pending_commands
         try:
-            data = os.read(self.fd, 4096)
-        except os.error:
+            data = str(os.read(self.fd, 4096).decode('utf8'))
+        except os.error, UnicodeDecodeError:
             logging.exception("Read g-code")
             return
         self.input_log.append((eventtime, data))
         self.bytes_read += len(data)
-        lines = data.decode().split('\n')
+        lines = data.split('\n')
         lines[0] = self.partial_input + lines[0]
         self.partial_input = lines.pop()
         pending_commands = self.pending_commands

commit 3a30dd2cf4248e5f92bdbaf096af00ad805edeac

gcode: Fix Python3 syntax error
Fix error introduced in d98b1a20.  Also, go back to only accepting
ascii characters on input.

Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

klippy/gcode.py
@@ -381,8 +381,8 @@ class GCodeIO:
     def _process_data(self, eventtime):
         # Read input, separate by newline, and add to pending_commands
         try:
-            data = str(os.read(self.fd, 4096).decode('utf8'))
-        except os.error, UnicodeDecodeError:
+            data = str(os.read(self.fd, 4096).decode())
+        except (os.error, UnicodeDecodeError):
             logging.exception("Read g-code")
             return
         self.input_log.append((eventtime, data))