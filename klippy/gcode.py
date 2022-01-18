Marirossibn/klipper

commit f27d488fc092687c24dff3bd62382d3603340df2

gcode_macro: Add "rawparams" pseudo-variable
Signed-off-by: Pedro Lamas <pedrolamas@gmail.com>

klippy/gcode.py
@@ -26,6 +26,22 @@ class GCodeCommand:
         return self._commandline
     def get_command_parameters(self):
         return self._params
+    def get_raw_command_parameters(self):
+        rawparams = self._commandline
+        command = self._command
+        urawparams = rawparams.upper()
+        if not urawparams.startswith(command):
+            start = urawparams.find(command)
+            end = rawparams.rfind('*')
+            if end >= 0:
+                rawparams = rawparams[:end]
+            rawparams = rawparams[start:]
+        commandlen = len(command) + 1
+        if len(rawparams) > commandlen:
+            rawparams = rawparams[commandlen:]
+        else:
+            rawparams = ''
+        return rawparams
     def ack(self, msg=None):
         if not self._need_ack:
             return False

klippy/extras/display_status.py
@@ -35,19 +35,8 @@ class DisplayStatus:
         curtime = self.printer.get_reactor().monotonic()
         self.expire_progress = curtime + M73_TIMEOUT
     def cmd_M117(self, gcmd):
-        msg = gcmd.get_commandline()
-        umsg = msg.upper()
-        if not umsg.startswith('M117'):
-            # Parse out additional info if M117 recd during a print
-            start = umsg.find('M117')
-            end = msg.rfind('*')
-            if end >= 0:
-                msg = msg[:end]
-            msg = msg[start:]
-        if len(msg) > 5:
-            self.message = msg[5:]
-        else:
-            self.message = None
+        msg = gcmd.get_raw_command_parameters() or None
+        self.message = msg
 
 def load_config(config):
     return DisplayStatus(config)

klippy/extras/gcode_macro.py
@@ -180,6 +180,7 @@ class GCodeMacro:
         kwparams = dict(self.variables)
         kwparams.update(self.template.create_template_context())
         kwparams['params'] = gcmd.get_command_parameters()
+        kwparams['rawparams'] = gcmd.get_raw_command_parameters()
         self.in_script = True
         try:
             self.template.run_gcode_from_command(kwparams)

klippy/extras/respond.py
@@ -22,17 +22,7 @@ class HostResponder:
         gcode.register_command('RESPOND', self.cmd_RESPOND, True,
                                desc=self.cmd_RESPOND_help)
     def cmd_M118(self, gcmd):
-        msg = gcmd.get_commandline()
-        umsg = msg.upper()
-        if not umsg.startswith('M118'):
-            # Parse out additional info if M118 recd during a print
-            start = umsg.find('M118')
-            end = msg.rfind('*')
-            msg = msg[start:end]
-        if len(msg) > 5:
-            msg = msg[5:]
-        else:
-            msg = ''
+        msg = gcmd.get_raw_command_parameters()
         gcmd.respond_raw("%s %s" % (self.default_prefix, msg))
     cmd_RESPOND_help = ("Echo the message prepended with a prefix")
     def cmd_RESPOND(self, gcmd):

commit a043ea95506f2d3a744931f707d740e29b012704

gcode: Handle M117 and M118 commands that start with a special character
Commit f27d488f broke the special handling for M117 commands that
start with a number or special character.  Fix that support and extend
to M118 as well.

Also improve handling of commands not separated by a space (eg,
"M117HELLO").

Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

klippy/gcode.py
@@ -27,20 +27,19 @@ class GCodeCommand:
     def get_command_parameters(self):
         return self._params
     def get_raw_command_parameters(self):
-        rawparams = self._commandline
         command = self._command
+        if command.startswith("M117 ") or command.startswith("M118 "):
+            command = command[:4]
+        rawparams = self._commandline
         urawparams = rawparams.upper()
         if not urawparams.startswith(command):
-            start = urawparams.find(command)
+            rawparams = rawparams[urawparams.find(command):]
             end = rawparams.rfind('*')
             if end >= 0:
                 rawparams = rawparams[:end]
-            rawparams = rawparams[start:]
-        commandlen = len(command) + 1
-        if len(rawparams) > commandlen:
-            rawparams = rawparams[commandlen:]
-        else:
-            rawparams = ''
+        rawparams = rawparams[len(command):]
+        if rawparams.startswith(' '):
+            rawparams = rawparams[1:]
         return rawparams
     def ack(self, msg=None):
         if not self._need_ack:
@@ -277,9 +276,9 @@ class GCodeDispatch:
             if cmdline:
                 logging.debug(cmdline)
             return
-        if cmd.startswith("M117 "):
-            # Handle M117 gcode with numeric and special characters
-            handler = self.gcode_handlers.get("M117", None)
+        if cmd.startswith("M117 ") or cmd.startswith("M118 "):
+            # Handle M117/M118 gcode with numeric and special characters
+            handler = self.gcode_handlers.get(cmd[:4], None)
             if handler is not None:
                 handler(gcmd)
                 return